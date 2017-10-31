a0db5e35f21b6df1a49d03419378beb4551ef729
Osvaldo Colina <osvaldo.colina@gmail.com>
2017-10-31 16:33:15 -0300

    Add exports.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,19 @@
 import Bubble from './components/Bubble';
+import Chat from './components/Chat';
+import Conversation from './components/Conversation';
+import Header from './components/Header';
+import Input from './components/Input';
+import Trigger from './components/Trigger';
+import Widget from './components/Widget';
+import Window from './components/Window';
 
 export {
-  Bubble
+  Bubble,
+  Chat,
+  Conversation,
+  Header,
+  Input,
+  Trigger,
+  Widget,
+  Window
 }
-
